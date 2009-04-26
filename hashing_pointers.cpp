87f07067d744f02ca90fbbc382cccafd069fe06c
troy <troy@zinc.(none)>
2009-04-26 10:13:05 -0400

    hashing pointers

---
+++ hashing_pointers.cpp
@@ -0,0 +1,17 @@
+namespace detail {
+    const unsigned nbuckets = 1024; 
+    const unsigned hash_mask = nbuckets-1; // 0x3FF if nbuckets is 1024
+
+    struct type_info_ptr_hash
+    {
+	size_t operator()( const std::type_info* p) const
+	{
+	    return (reinterpret_cast<size_t>(p) >> sizeof(void*)) & hash_mask;
+	}
+    };
+}
+
+std::unordered_map<const std::type_info*, 
+		   std::string, 
+		   detail::type_info_ptr_hash>  storage_t;
+
