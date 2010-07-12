405ba10f364069c6d7bb89dcb9fdd89056594c87
Eric Niebler <eric@boostpro.com>
2010-07-11 23:50:57 -0400

    Functional script for finding differences between a live boost repo and the modularization manifest. Fix up the manifest to bring it in sync with the latest boost trunk.

--- boost/findmoddiffs.py
+++ boost/findmoddiffs.py
@@ -1,4 +1,4 @@
-import os, sys, getopt, stat, pickle
+import os, sys, getopt, stat, pickle, posixpath
 from subprocess import PIPE, Popen
 
 boost_manifest_pkl = 'boost_manifest.cache'
@@ -6,17 +6,17 @@ verbose = False
 
 # Print the usage message (there's probably a nifty python way to do this)
 def usage():
-    print '''Usage: findmoddiffs.py [-h] [-a] [-v] [--live-boost-repo=<dir>]
-    -h                  : help
-    -v                  : verbose
-    -a                  : always regenerate working files
-    --live-boost-repo   : path to the git repository storing the unmodularized boost'''
+    print '''Usage: findmoddiffs.py [-h] [-a] [-v] [--source=<dir>]
+    -h          : help
+    -v          : verbose
+    -a          : always regenerate working files
+    --source    : path to the git repository storing the unmodularized boost'''
 
 # Generate the boost file manifest from the live git repo
 # found at live_boost_repo_dir
 def gen_manifest(live_boost_repo_dir):
     if not live_boost_repo_dir:
-        print 'Error: ', boost_manifest_pkl, ' not found and --live-boost-repo not specified'
+        print 'Error: ', boost_manifest_pkl, ' not found and --source not specified'
         usage()
         sys.exit(2)
     if not os.path.isdir(os.path.normpath(os.path.join(live_boost_repo_dir, '.git'))):
@@ -33,13 +33,15 @@ def gen_manifest(live_boost_repo_dir):
     o = Popen(['git ls-files libs'], \
         stdout=PIPE, cwd=live_boost_repo_dir, shell=True).communicate()[0]
     files.extend(o.split('\n'))
-    with open(boost_manifest_pkl, 'wb') as manifest:
-        pickle.dump([f for f in files if f != ''], manifest, -1)
+    with open(boost_manifest_pkl, 'wb') as input:
+        existing = [f for f in files if f != '']
+        existing.sort()
+        pickle.dump(existing, input, -1)
 
 def main():
     # Parse the command line arguments
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ahv", ["help", "live-boost-repo="])
+        opts, args = getopt.getopt(sys.argv[1:], "ahv", ["help", "source="])
     except getopt.GetoptError, err:
         print 'Error: ', str(err)
         usage()
@@ -55,7 +57,7 @@ def main():
         elif o in ("-h", "--help"):
             usage()
             sys.exit()
-        elif o == "--live-boost-repo":
+        elif o == "--source":
             live_boost_repo_dir = a
         else:
             assert False, "unhandled option"
@@ -68,17 +70,90 @@ def main():
     # Load the boost file manifest from the pickle
     try:
         # Try to read the pickle
-        with open(boost_manifest_pkl, 'rb') as manifest:
-            files = pickle.load(manifest)
+        with open(boost_manifest_pkl, 'rb') as input:
+            existing = pickle.load(input)
     except IOError:
         # pickle wasn't there, generate it:
         gen_manifest(live_boost_repo_dir)
-        with open(boost_manifest_pkl, 'rb') as manifest:
-            files = pickle.load(f)
-
+        with open(boost_manifest_pkl, 'rb') as input:
+            existing = pickle.load(input)
+    
     if verbose:
         print '[INFO] Boost manifest:'
-        print '[INFO]     ' + '\n[INFO]     '.join(files)
+        print '[INFO]     ' + '\n[INFO]     '.join(existing)
 
+    try:
+        # Read the manifest containing the modularization mappings
+        with open('manifest.txt', 'r') as input:
+            # Parse lines like "R100 some/src/path/ some/dst/path/" into
+            # a sorted list of "some/src/path/"
+            #manifest = [os.path.normcase(line.split('\t')[1]) for line in input if line[:4] == 'R100']
+            manifest = [line.split('\t')[1] for line in input if line[:4] == 'R100']
+            manifest.sort()
+    except IOError:
+        print 'Error: can not find file manifest.txt'
+        sys.exit(1)
+
+    # Make "manifest" and "existing" essentially a map of paths to counts.
+    # Use lists instead of dictionaries to keep ordering intact, and
+    # use nested lists instead of tuples so we can modify the count
+    # in-place.
+    manifest = [[f,0] for f in manifest]
+    existing = [[f,0] for f in existing]
+
+    fsi = manifest.__iter__()
+    exi = existing.__iter__()
+
+    # Iterate over the manifast and the existing files in lock step
+    # advancing one or the other or both as necessary and keeping
+    # track in the counts which paths appear in both lists and which
+    # don't.
+    try:
+        f = fsi.next()
+        e = exi.next()
+        while 1:
+	    # If the paths are the same, (e.g. they're both 'foo/bar.hpp')
+	    # then inc the count for each and advance both iterators
+            if f[0] == e[0]:
+                f[1] += 1
+                e[1] += 1
+                f = fsi.next()
+                e = exi.next()
+	    # If the path as listed in the manifest sorts after the
+	    # current path in the repo, advance the repo's iterator
+	    # so it catches up.
+            elif f[0] > e[0]:
+                e = exi.next()
+	    # If the manifest path is a directory and the repo path
+	    # represents a file in that directory, in the count for
+	    # both, but only advance the repo iterator. There may
+	    # yet me more files in this directory to count.
+            elif f[0][-1] == '/' and e[0].startswith(f[0]):
+                f[1] += 1
+                e[1] += 1
+                e = exi.next()
+	    # Otherwise, the manifest path sorts before the repo path
+	    # and is not a parent directory of the repo path, so advance
+	    # the manifest iterator so it catches up.
+            else:
+                f = fsi.next()
+    except StopIteration:
+        pass
+
+    # Display the files that are in the repo that are
+    # not accounted for in the manifest.
+    print '\nChecking existing files against manifest ...'
+    for e in existing:
+        if 0 == e[1]:
+            print '\tmissed', e[0]
+    
+    # Display the files and directories in the manifest that
+    # have no correspondence with files or directories in the
+    # repo.
+    print '\nChecking manifest against existing files ...'
+    for f in manifest:
+        if 0 == f[1]:
+            print '\tmissed', f[0]
+        
 if __name__ == "__main__":
     main()
