d4615e05ad0830de2ec1283c490738ce6950dd03
Eric Niebler <eric@boostpro.com>
2010-08-15 18:50:16 -0400

    add stubs that will let us customize how ryppl handles build and install commands

--- src/ryppl/canonical_setup.py
+++ src/ryppl/canonical_setup.py
@@ -8,6 +8,22 @@ if use_distutils2:
     from distutils2 import setup
 else:
     from setuptools import setup
+    from setuptools.command.install import install
+    from distutils.command.build import build
+
+    # setuptools command object to execute during ryppl build
+    class cmake_build(build):
+      def run (self):
+        print "cmake_build!!!"
+        pass
+
+    # setuptools command object to execute during ryppl install
+    class cmake_install(install):
+      def run (self):
+        print "cmake_install!!!"
+        # Must build before we can install
+        self.run_command('build')
+        pass
 
 # Read the metadata out of the project's .ryppl/METADATA file
 metadata = DistributionMetadata(
@@ -36,12 +52,73 @@ def metadata_to_setup_keywords(metadata):
 
         m = item_to_attribute(metadata)
         # There's probably a more declarative way, but until then, I just
-        # copied these keywords out of the distutils source
+        # copied these keywords out of the distutils source.
+        # EAN: these parameters are documented at
+        # http://docs.python.org/distutils/apiref.html
         return dict(
+          # Meaning: The Distribution class to use.
+          # Type: A subclass of distutils.core.Distribution
           # 'distclass': ???
+          
+          # Meaning: The name of the setup.py script - defaults to sys.argv[0].
+          # Type: a string
           # 'script_name':???
+          
+          # Meaning: Arguments to supply to the setup script.
+          # Type: a list of strings
           # 'script_args', ???
+          
+          # Meaning: default options for the setup script.
+          # Type: a string
           # 'options', ???
+
+          # Copied from setuputils/dist.py. Looks like these could be useful.
+          #'extras_require' -- a dictionary mapping names of optional "extras" to the
+          #     additional requirement(s) that using those extras incurs. For example,
+          #     this::
+          #
+          #     extras_require = dict(reST = ["docutils>=0.3", "reSTedit"])
+          #
+          #     indicates that the distribution can optionally provide an extra
+          #     capability called "reST", but it can only be used if docutils and
+          #     reSTedit are installed.  If the user installs your package using
+          #     EasyInstall and requests one of your extras, the corresponding
+          #     additional requirements will be installed if needed.
+          # 
+          #'features' -- a dictionary mapping option names to 'setuptools.Feature'
+          #   objects.  Features are a portion of the distribution that can be
+          #   included or excluded based on user options, inter-feature dependencies,
+          #   and availability on the current system.  Excluded features are omitted
+          #   from all setup commands, including source and binary distributions, so
+          #   you can create multiple distributions from the same source tree.
+          #   Feature names should be valid Python identifiers, except that they may
+          #   contain the '-' (minus) sign.  Features can be included or excluded
+          #   via the command line options '--with-X' and '--without-X', where 'X' is
+          #   the name of the feature.  Whether a feature is included by default, and
+          #   whether you are allowed to control this from the command line, is
+          #   determined by the Feature object.  See the 'Feature' class for more
+          #   information.
+          #
+          #'test_suite' -- the name of a test suite to run for the 'test' command.
+          #   If the user runs 'python setup.py test', the package will be installed,
+          #   and the named test suite will be run.  The format is the same as
+          #   would be used on a 'unittest.py' command line.  That is, it is the
+          #   dotted name of an object to import and call to generate a test suite.
+          #
+          #'package_data' -- a dictionary mapping package names to lists of filenames
+          #   or globs to use to find data files contained in the named packages.
+          #   If the dictionary has filenames or globs listed under '""' (the empty
+          #   string), those names will be searched for in every package, in addition
+          #   to any names for the specific package.  Data files found using these
+          #   names/globs will be installed along with the package, in the same
+          #   location as the package.  Note that globs are allowed to reference
+          #   the contents of non-package subdirectories, as long as you use '/' as
+          #   a path separator.  (Globs are automatically converted to
+          #   platform-specific paths at runtime.)
+
+          # This hooks the "build" and "install" commands to do our bidding.
+          cmdclass = {'build': cmake_build, 'install': cmake_install},
+
           name = m.name,
           version = m.version,
           author = m.author,
