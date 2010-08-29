27f85f08f5728e638ccb985fb043e0b1ebd3101b
Eric Niebler <eric@boostpro.com>
2010-08-29 09:31:44 -0400

    add test for ryppl uninstall

--- test/test_basic.py
+++ test/test_basic.py
@@ -4,6 +4,7 @@ from pkgtest import mktree
 from urllib import pathname2url
 from cStringIO import StringIO
 from ryppl_test import Environment as new_test, create_projects
+from testutil import mkdtemp, _temp_dirs, exe_ext as EXE
 
 here = Path(__file__).abspath.folder
 distutils2_src = here.folder/'submodule'/'distutils2'/'src'
@@ -15,6 +16,19 @@ except ImportError:
     import distutils2
 
 from distutils2.metadata import DistributionMetadata as METADATA
+
+# This scripts gets found and run by nosetests because its name matches nosetests
+# naming conventions. Also, the functions below of the form test_xxx() are found
+# and executed as individual tests for the same reason. 
+#
+# Each test gets run in its own virtualenv. The creation of a new Environment
+# object (by new_text()) causes the creation of the virtualenv. That causes the
+# os.environ['PATH'] to be updated so that the virtualenv's python is the one
+# that is found. Any external python scripts executed by launching a separate
+# instance of python will use that virtualenv. For tnat invocation of python,
+# the libs, site-packages and bin paths all point to that virtualenv. THAT MAKES
+# IT BEHAVE DIFFERENTLY THEN THE CURRENT INVOCATION OF PYTHON WHICH IS IN A
+# DIFFERENT VIRTUALENV.
 
 def test_fetch():
     env = new_test()
@@ -42,6 +56,8 @@ def test_diamond():
           # '--no-install', 
           '-vvv', '-i', index, 'ProjA')
 
+# Test that a project with a CMakeLists.txt gets built and installed correctly
+# by ryppl
 def test_cmake():
     env = new_test()
     index,projects = create_projects(
@@ -66,15 +82,50 @@ int main() {
 ''')
     env.ryppl('install', '-vvv', '-i', index, 'greet')
     result = env.run('greet')
-    #print >> sys.stderr, result.stdout
     if not result.stdout == 'hello ryppl\n':
         raise AssertionError(
             'Unexpected output from greet executable. Expected "%s", got "%s"'
                 % ('hello ryppl\\n', result.stdout))
 
+# Test that a CMake project can be installed and uninstalled correctly by ryppl
+def test_uninstall():
+    env = new_test()
+    index,projects = create_projects(
+        env,
+        empty=dict())
+
+    empty = projects['empty']
+    empty.add_file('CMakeLists.txt', '''
+cmake_minimum_required(VERSION 2.8)
+project (empty)
+add_executable(empty empty.cpp)
+install (TARGETS empty 
+  RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}
+)
+''')
+
+    empty.add_file('empty.cpp', 'int main() {\n}\n')
+
+    # create a temporary path. We'll install the exe here.
+    runtime_dst = Path(env.bin_path)/'empty-bin'
+    install_option = '--install-option=--install-scripts='+runtime_dst
+
+    # install the package and confirm that the file exists
+    # in the place we expect it to
+    env.ryppl('install', '-vvv', install_option, '-i', index, 'empty')
+    if not os.path.isfile(runtime_dst/('empty'+EXE)):
+        raise AssertionError(
+            'Failed to find installed file at "%s"' % (runtime_dst/('empty'+EXE)))
+
+    # uninstall the packageand confirm the file as been deleted
+    env.ryppl('uninstall', '-vvv', '-y', 'empty')
+    if os.path.isfile(runtime_dst/('empty'+EXE)):
+        raise AssertionError(
+            'Failed to uninstall file at "%s"' % (runtime_dst/('empty'+EXE)))
 
 
 if __name__ == '__main__':
     test_diamond()
     test_cmake()
-    
+    test_uninstall()
+
